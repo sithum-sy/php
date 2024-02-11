b89982386a92f43b06483dc77559c7d7aefd91b7
sithum-sy <sithum.sy@gmail.com>
2024-02-11 08:23:28 +0530

    01.28.2024

---
+++ phpcs/test4.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Apress\PhpDevTools;
+
+class MyClass
+{
+    const VERSION = '1.0';
+
+    public $myProperty;
+
+    public function myMethod()
+    {
+        $this->myProperty = true;
+    }
+}
