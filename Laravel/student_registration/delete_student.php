f8f037f6526c1ad893319ed2a215202b5943b54e
sithum-sy <sithum.sy@gmail.com>
2024-04-06 10:09:06 +0530

    4/06/2024 - Morning

---
+++ Laravel/student_registration/delete_student.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("Location: login-form.php");
+    exit;
+}
+
+if(isset($_POST['id'])) {
+
+    require_once "db_connection.php";
+
+    $stmt = $conn->prepare("DELETE FROM students WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $id = $_POST['id'];
+
+    if ($stmt->execute()) {
+        header("Location: home.php");
+        exit;
+    } else {
+        header("Location: home.php?error=delete_failed");
+        exit;
+    }
+
+    $stmt->close();
+    $conn->close();
+} else {
+    header("Location: home.php?error=id_not_set");
+    exit;
+}
+?>
