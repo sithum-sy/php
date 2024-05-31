45a1e6fcceed5084bc196417471ad9e7a7a5367c
sithum-sy <sithum.sy@gmail.com>
2024-05-31 23:28:29 +0530

    5/31/2024 - Navbar href linked

--- Laravel/publication-and-author-management-system/resources/views/welcome.blade.php
+++ Laravel/publication-and-author-management-system/resources/views/welcome.blade.php
@@ -62,13 +62,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link " href="#">Login</a>
+                        <a class="nav-link " href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Register</a>
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Publication Register</a>
+                        <a class="nav-link" href="{{ route('publication-register') }}">Publication Register</a>
                     </li>
                 </ul>
             </div>
